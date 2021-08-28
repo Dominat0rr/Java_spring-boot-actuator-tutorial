90ff339bf4ffa0d3af352cc89e876003636a8564
Dominat0rr <claermandominik@gmail.com>
2021-08-28 16:27:19 +0200

    setup

---
+++ src/main/java/be/domain/app/SpringBootActuatorTutorialApplication.java
@@ -0,0 +1,13 @@
+package be.domain.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActuatorTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootActuatorTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/domain/app/SpringBootActuatorTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package be.domain.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActuatorTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
